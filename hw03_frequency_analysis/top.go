21031379c8e246ac2dd1f40503469417bbe65f78
tabularasa31 <tabularasa31@gmail.com>
2022-06-21 22:44:28 +0300

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type Words struct {
+	Word  string
+	Count int
+}
+
+func Top10(s string) []string {
+	w := []Words{}
+	wordMap := make(map[string]int)
+
+	for _, word := range strings.Fields(s) {
+		wordMap[word]++
+	}
+
+	for key, val := range wordMap {
+		var temp Words
+		temp.Word = key
+		temp.Count = val
+		w = append(w, temp)
+	}
+
+	sort.Slice(w, func(i, j int) bool {
+		return w[i].Count > w[j].Count || (w[i].Count == w[j].Count && (strings.Compare(w[i].Word, w[j].Word) < 0))
+	})
+
+	res := []string{}
+
+	for _, val := range w {
+		res = append(res, val.Word)
+	}
+
+	if len(res) < 11 {
+		return res
+	}
+
+	return res[:10]
 }
