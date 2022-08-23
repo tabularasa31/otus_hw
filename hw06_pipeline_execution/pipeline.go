6e67b29f54103971fd220a967b4eb7499bb5ed08
tabularasa31 <tabularasa31@gmail.com>
2022-08-23 21:36:12 +0300

    first commit

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,15 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	select {
+	case <-done:
+		return out
+	case <-in:
+		for _, stage := range stages {
+			out = stage(out)
+		}
+	}
+
+	return out
 }
