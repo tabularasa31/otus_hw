495f38a1830a32f08c0a36f193070f91b4b89677
tabularasa31 <tabularasa31@gmail.com>
2022-09-25 15:03:38 +0300

    hw11  commit

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -13,8 +13,6 @@ import (
 )
 
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 
 	timeout, addr := handleFlags()
 
