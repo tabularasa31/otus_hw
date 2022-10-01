203e8ab2f381ecd24c7d8dbfcbd760cbaef97e3d
tabularasa31 <tabularasa31@gmail.com>
2022-10-01 11:49:08 +0300

    hw12 initial commit

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -8,10 +8,10 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+	"github.com/tabularasa31/hw_otus/hw12_13_14_15_calendar/internal/app"
+	"github.com/tabularasa31/hw_otus/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/tabularasa31/hw_otus/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/tabularasa31/hw_otus/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw12_13_14_15_calendar
+module github.com/tabularasa31/hw_otus/hw12_13_14_15_calendar
 
 go 1.16
