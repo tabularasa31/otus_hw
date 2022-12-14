fee0ce0b02c59dad69005d5ce2e96355c29e3b13
tabularasa <tabularasa31@email.com>
2022-12-15 00:17:09 +0300

    Add Makefile

--- hw12_13_14_15_calendar/migrations/00001_init.sql
+++ hw12_13_14_15_calendar/migrations/00001_init.sql
@@ -1,5 +1,6 @@
 -- +goose Up
-CREATE TABLE events
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS events
 (
     id           serial primary key,
     title        text not null ,
@@ -16,12 +17,14 @@ CREATE INDEX user_id_idx
 CREATE INDEX event_time_idx
     ON events (event_time);
 
-INSERT INTO events VALUES
+INSERT INTO events(user_id, title, descr, event_time, duration, notification)
+VALUES
     (42, 'new year', 'Happy New Year!', '2019-12-31 23:59:59', '01:00:00', '2019-12-31 23:59:50'),
     (43, 'new year 2', 'Happy New Year 22!', '2019-12-31 23:59:59', '01:00:00', '2019-12-31 23:59:50'),
     (47, 'happy birthday', 'Its a new date', '2020-05-31 09:00:00', '01:30:00', '2020-05-31 08:00:00'),
     (48, 'happy birthday 2', 'Its a new date 22', '2020-06-05 09:00:00', '01:30:00', '2020-06-05 08:00:00');
+-- +goose StatementEnd
 
 
 -- +goose Down
-DROP TABLE events;
+DROP TABLE IF EXISTS events;
